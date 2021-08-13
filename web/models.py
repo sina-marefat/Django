24a419fb49d032d155cbbb089573cfd5c304ee9d
Sina Marefat <Sinamatefat@gmail.com>
2021-08-13 20:20:27 +0430

    first model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Education(models.Model):
+    university = models.CharField(max_length=100)
+    term = models.IntegerField()
+    enterYear = models.IntegerField()
+    payOrFree = models.BooleanField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
